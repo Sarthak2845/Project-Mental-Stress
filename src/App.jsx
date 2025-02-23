2f01ec25c28efdf7a155b2e89fb4e3f7c782a105
Avinash <abhiapril122005@gmail.com>
2025-02-23 12:10:43 +0530

    avi initail commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
+import { useState } from "react";
+import Preloader from "./components/Preloader";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import "./index.css"
+const App = () => {
+  const [loading, setLoading] = useState(true);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className="min-h-screen w-full bg-gradient-to-br from-gray-900 to-black text-white flex flex-col items-center text-center p-6">
+      {loading ? (
+        <Preloader onFinish={() => setLoading(false)} />
+      ) : (
+        <>
+          <Navbar />
+          <Home />
+          <div className="bg-blue-500 text-white text-center p-4">
+      Tailwind is working! 🚀
+    </div>
 
-export default App
+        </>
+
+      )}
+    </div>
+  )}
+
+export default App;
